5de35198a2c2c43ee196d52a750ee1fdcd9ed7c0
jmcparland <jmcparland@tcmlabs.com>
2018-01-25 18:20:11 -0500

    Update recover-pwfail.sh

--- recover-pwfail.sh
+++ recover-pwfail.sh
@@ -5,5 +5,5 @@ VPN="openvpn@overwatch"
 iptables -P INPUT DROP
 systemctl disable $VPN
 systemctl stop openvpn
-systemctl disable sshd
-systemctl stop sshd
+systemctl disable ssh
+systemctl stop ssh
